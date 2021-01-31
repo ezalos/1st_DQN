0b096a2b27651a5e8d3460711d717de912945e65
ezalos <ezalos@gmail.com>
2021-01-31 15:03:16 +0100

    Start

--- test_gym.py
+++ test_gym.py
@@ -2,6 +2,10 @@ from cartpole import CartPoleEnv
 
 if __name__ == "__main__":
     cart = CartPoleEnv()
+    cart.reset()
     while True:
         cart.render()
+        cart.step(0)
+        cart.render()
         cart.step(1)
+    cart.close()
