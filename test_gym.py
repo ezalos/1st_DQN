e339f41a32c3efc1cfab3245ce0d6bbf3f9b8a3d
Joep B <joepbarmentlo@gmail.com>
2021-01-31 16:14:36 +0100

    config file

--- test_gym.py
+++ test_gym.py
@@ -3,22 +3,5 @@ import gym
 import numpy as np
 
 if __name__ == "__main__":
-    cart = CartPoleEnv()
-    cart.reset()
-    # while True:
-    #     cart.render()
-    #     cart.step(0)
-    #     cart.render()
-    #     cart.step(1)
-    a = cart.step(1)
-    print(a)
-    cart.close()
-    env = gym.make("CartPole-v1")
-# print(env.action_space.n)
-#     # print(env.action_space.n)
-#     # Observation = [30,30,50,50]
-
-#     # q_table = np.random.uniform(low=0, high=1, size=(Observation + [env.action_space.n]))
-#     # print(q_table.shape)
 
 
